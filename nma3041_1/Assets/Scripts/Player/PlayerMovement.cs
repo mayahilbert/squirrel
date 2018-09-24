d7fb61fa393b4ce0d45e22953fb0ee66066f8a35
Maya <35238760+mayahilbert@users.noreply.github.com>
2018-09-23 21:44:19 -0400

    third update

--- nma3041_1/Assets/Scripts/Player/PlayerMovement.cs
+++ nma3041_1/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,23 +3,21 @@
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 6f;            // The speed that the player will move at.
-    public float jumpSpeed = 3f;
+    public float jumpspeed = 5f;        // The speed that the player will jump with.
     Vector3 movement;                   // The vector to store the direction of the player's movement.
-    //Animator anim;                      // Reference to the animator component.
+    Animator anim;                      // Reference to the animator component.
     Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+    public bool onGround;               // Check if player is on the ground.
+
 
     void Awake()
     {
-         //Create a layer mask for the floor layer.
-         floorMask = LayerMask.GetMask("Floor");
-
         // Set up references.
-       // anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody>();
-    }
+        onGround = true;
 
+    }
 
     void FixedUpdate()
     {
@@ -30,16 +28,16 @@ public class PlayerMovement : MonoBehaviour
         // Move the player around the scene.
         Move(h, v);
 
-        if (Input.GetKeyDown("space"))
-        {
-            transform.Translate(Vector3.up * jumpSpeed * Time.deltaTime, Space.World);
-        }
-
         // Turn the player to face the mouse cursor.
-        // Turning();
+        // Turn the player to face the direction of keyboard key pressed.
+        Turning();
 
         // Animate the player.
-        // Animating(h, v);
+        Animating(h, v);
+
+        //Allow player to jump with spacebar.
+        Jumping();
+
     }
 
     void Move(float h, float v)
@@ -53,30 +51,15 @@ public class PlayerMovement : MonoBehaviour
         // Move the player to it's current position plus the movement.
         playerRigidbody.MovePosition(transform.position + movement);
     }
-
-    /*void Turning()
+    
+    void Turning()
     {
-        // Create a ray from the mouse cursor on screen in the direction of the camera.
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
 
-        // Create a RaycastHit variable to store information about what was hit by the ray.
-        RaycastHit floorHit;
-
-        // Perform the raycast and if it hits something on the floor layer...
-        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
-        {
-            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
-            Vector3 playerToMouse = floorHit.point - transform.position;
-
-            // Ensure the vector is entirely along the floor plane.
-            playerToMouse.y = 0f;
-
-            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-
-            // Set the player's rotation to this new rotation.
-            playerRigidbody.MoveRotation(newRotation);
-        }
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (movement != Vector3.zero)
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
     }
 
     void Animating(float h, float v)
@@ -86,5 +69,22 @@ public class PlayerMovement : MonoBehaviour
 
         // Tell the animator whether or not the player is walking.
         anim.SetBool("IsWalking", walking);
-    }*/
+    }
+
+    void Jumping()
+    {
+        if(onGround && Input.GetButtonDown("Jump"))
+        {
+            playerRigidbody.velocity = new Vector3(0f, 5f, 0f);
+            onGround = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision any)
+    {
+        any.gameObject.CompareTag("Ground");
+        onGround = true;
+
+    }
 }
