c672d2b7f128d977d92665dc9b64c6e7dab24011
Maya <35238760+mayahilbert@users.noreply.github.com>
2018-09-29 22:17:09 -0400

    added camera controls

--- nma3041_1/Assets/CameraFollow.cs
+++ nma3041_1/Assets/CameraFollow.cs
@@ -8,12 +8,23 @@ public class CameraFollow : MonoBehaviour {
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
 	// Use this for initialization
 	void LateUpdate () {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-        transform.position = smoothedPosition;
+       // Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = desiredPosition;
         transform.LookAt(target);
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 	}
 	
 }
